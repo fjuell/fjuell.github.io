6dd69e76e9e20ee8625ff88608aeb82934d4bb21
Fredrik H. Juell <fjuell@users.noreply.github.com>
2016-03-17 21:12:53 +0100

    Added files via upload

---
+++ bookmarks.js
@@ -0,0 +1,7 @@
+
+
+function changeClass() {
+	document.getElementById("fhead").className="w3-container w3-ana-aircraft-black";
+	document.getElementById("body").className="w3-ana-instrument-black";
+	document.getElementById("skranke").className="w3-btn w3-ana-navy-blue w3-hover-amber w3-btn-block";
+}
