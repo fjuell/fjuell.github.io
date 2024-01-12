995f06cc773d9ad826c681262ba30b9775ba6aaa
Fredrik H. Juell <fjuell@users.noreply.github.com>
2024-01-12 14:42:33 +0100

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,9 +1,9 @@
 // Code from https://p5js.org/examples/interaction-wavemaker.html
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 50);
   noStroke();
-  fill(40, 200, 40);
+  fill(40, 300, 40);
 }
 
 function draw() {
@@ -11,7 +11,7 @@ function draw() {
   i = 0
   j = 0
   // make a x and y grid of ellipses
-  for (let x = 0; x <= width; x = x + 30) {
+  for (let x = 0; x <= width; x = x + 50) {
     for (let y = 0; y <= height; y = y + 30) {
       // starting point of each circle depends on mouse position
       const xAngle = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
@@ -21,11 +21,11 @@ function draw() {
 
       // each particle moves in a circle
       let t = frameCount * 0.008;
-      const myX = x + 40 * cos(2 * PI * t + angle);
-      const myY = y + 40 * sin(3 * PI * t + angle);
+      const myX = x + 20 * cos(2 * PI * t + angle);
+      const myY = y + 20 * sin(5 * PI * t + angle);
       
       fill(j, 200, i);
-      circle(myX, myY, 6); // draw particle
+      circle(myX, myY, 2); // draw particle
     }
     i = i + 50
     j = j + 30
