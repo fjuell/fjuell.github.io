c91e8c512e52c732a503729fb9ae22a4821624eb
Fredrik H. Juell <fjuell@users.noreply.github.com>
2024-02-05 09:33:53 +0100

    Add files via upload

--- bm.js
+++ bm.js
@@ -1,7 +1,38 @@
 
 
-function changeClass() {
-	document.getElementById("fhead").className="w3-container w3-ana-aircraft-black";
-	document.getElementById("body").className="w3-ana-instrument-black";
-	document.getElementById("skranke").className="w3-btn w3-ana-navy-blue w3-hover-amber w3-btn-block";
+// Initialize a variable to track the state
+let isOn = false;
+
+// Function to handle the click event and toggle the state
+function toggleButton(event) {
+  isOn = !isOn; // Toggle the state
+  if (isOn) {
+    // Perform actions when the button is "on"
+    console.log('Button is on');
+  } else {
+    // Perform actions when the button is "off"
+    console.log('Button is off');
+  }
+}
+
+// Assign the function to the button's click event using addEventListener
+const button = document.querySelector('button');
+button.addEventListener('click', toggleButton);
+
+
+
+
+function showSkranke() {
+    var elements = document.querySelectorAll("#skranke");
+    for (var i=0; i<elements.length; i++) {
+        elements[i].className = "w3-btn w3-border fdm-border-cyan fdm-highlight fdm-hover w3-btn-block";
+    }
+};
+
+
+function showProgrammering() {
+    var elements = document.querySelectorAll("#programmering");
+    for (var i=0; i<elements.length; i++) {
+        elements[i].className = "w3-btn w3-border fdm-border-green fdm-green fdm-hover w3-btn-block";
+    }
 }
