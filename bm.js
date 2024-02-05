1f2268873b008132a7f1b7758f7b3b45bc33f99f
Fredrik H. Juell <fjuell@users.noreply.github.com>
2024-02-05 12:30:57 +0100

    Add files via upload

--- bm.js
+++ bm.js
@@ -1,38 +1,54 @@
 
+let isSkrankeOn = false;
+let isProgOn = false;
 
-// Initialize a variable to track the state
-let isOn = false;
 
-// Function to handle the click event and toggle the state
-function toggleButton(event) {
-  isOn = !isOn; // Toggle the state
-  if (isOn) {
-    // Perform actions when the button is "on"
-    console.log('Button is on');
-  } else {
-    // Perform actions when the button is "off"
-    console.log('Button is off');
-  }
-}
+function toggleButtonSkranke() {
+    if (isSkrankeOn === false) {
+        isSkrankeOn = true;
+    }  else {
+        isSkrankeOn = false;
+    }
+};
 
-// Assign the function to the button's click event using addEventListener
-const button = document.querySelector('button');
-button.addEventListener('click', toggleButton);
-
+function toggleButtonProg() {
+    if (isProgOn === false) {
+        isProgOn = true;
+    } else {
+        isProgOn = false;
+    }
+};
 
 
 
 function showSkranke() {
     var elements = document.querySelectorAll("#skranke");
-    for (var i=0; i<elements.length; i++) {
-        elements[i].className = "w3-btn w3-border fdm-border-cyan fdm-highlight fdm-hover w3-btn-block";
+    if (isSkrankeOn === false) {
+        for (var i=0; i<elements.length; i++) {
+            elements[i].className = "w3-btn w3-border fdm-border-cyan fdm-highlight fdm-hover w3-btn-block";
+        }
+        toggleButtonSkranke();
+    } else {
+        for (var i=0; i<elements.length; i++) {
+            elements[i].className = "w3-btn w3-border-bottom fdm-border-blue fdm-highlight fdm-hover w3-btn-block";
+        }
+        toggleButtonSkranke();
     }
 };
 
 
 function showProgrammering() {
     var elements = document.querySelectorAll("#programmering");
-    for (var i=0; i<elements.length; i++) {
-        elements[i].className = "w3-btn w3-border fdm-border-green fdm-green fdm-hover w3-btn-block";
+    if (isProgOn === false) {
+        for (var i=0; i<elements.length; i++) {
+            elements[i].className = "w3-btn w3-border fdm-border-green fdm-green fdm-hover w3-btn-block";
+        }
+        toggleButtonProg();
+    } else {
+        for (var i=0; i<elements.length; i++) {
+            elements[i].className = "w3-btn w3-border-bottom fdm-border-blue fdm-green fdm-hover w3-btn-block";
+        }
+        toggleButtonProg();
     }
-}
+};
+
